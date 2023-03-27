c5e0bd512faecc78962bfdc021db7f0482fa4c8e
spamdalfz <coreyvasser@gmail.com>
2023-03-26 22:10:09 -0700

    added function to generate markdown for README.

--- index.js
+++ index.js
@@ -2,72 +2,71 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of your project?',
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Please provide a description of your project:',
-        },
-        {
-            type: 'input',
-            name: 'installation',
-            message: 'Please provide installation instructions for your project:',
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Please provide usage information for your project:',
-        },
-        {
-            type: 'input',
-            name: 'contributing',
-            message: 'Please provide contribution guidelines for your project:',
-        },
-        {
-            type: 'input',
-            name: 'tests',
-            message: 'Please provide test instructions for your project:',
-        },
-        {
-            type: 'list',
-            name: 'license',
-            message: 'Which license does your project use?',
-            choices: ['MIT', 'GPLv3', 'Apache', 'BSD', 'Unlicense'],
-        },
-        {
-            type: 'input',
-            name: 'username',
-            message: 'What is your GitHub username?',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email address?',
-        },
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What is the title of your project?',
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Please provide a description of your project:',
+            },
+            {
+                type: 'input',
+                name: 'installation',
+                message: 'Please provide installation instructions for your project:',
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: 'Please provide usage information for your project:',
+            },
+            {
+                type: 'input',
+                name: 'contributing',
+                message: 'Please provide contribution guidelines for your project:',
+            },
+            {
+                type: 'input',
+                name: 'tests',
+                message: 'Please provide test instructions for your project:',
+            },
+            {
+                type: 'list',
+                name: 'license',
+                message: 'Which license does your project use?',
+                choices: ['MIT', 'GPLv3', 'Apache', 'BSD', 'Unlicense'],
+            },
+            {
+                type: 'input',
+                name: 'username',
+                message: 'What is your GitHub username?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is your email address?',
+            },
 
-    ])
-    .then((answers) => {
-        const readmeData = generateMarkdown(answers);
-        writeToFile('README.md', readmeData);
-    });
+        ])
+        .then((answers) => {
+            const readmeData = generateMarkdown(answers);
+            writeToFile('README.md', readmeData);
+        });
 
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-        if (err) {
-            throw err;
-        }
-        console.log('README file has been saved!');
-    });
+    function writeToFile(fileName, data) {
+        fs.writeFile(fileName, data, (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log('README file has been saved!');
+        });
+    }
 }
-
-// TODO: Create a function to initialize app
-function init() { }
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,8 +13,37 @@ function renderLicenseSection(license) { }
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## License
+  This project is licensed under the ${data.license} license.
+  
+  ## Contributing
+  ${data.contributing}
+  
+  ## Tests
+  ${data.tests}
+  
+  ## Questions
+  If you have any questions, you can reach out to me on [GitHub](https://github.com/${data.username}) or contact me directly at ${data.email}.
+  `;
 }
+
+
 
 module.exports = generateMarkdown;
