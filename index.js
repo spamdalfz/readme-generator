78d7a4647a4e2b147c2887421fa707fb80f024ed
spamdalfz <coreyvasser@gmail.com>
2023-03-26 20:44:18 -0700

    added inquirer prompt with all the questions related to a professional readme/

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a description of your project:',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please provide installation instructions for your project:',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide usage information for your project:',
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Please provide contribution guidelines for your project:',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Please provide test instructions for your project:',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Which license does your project use?',
+            choices: ['MIT', 'GPLv3', 'Apache', 'BSD', 'Unlicense'],
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your GitHub username?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        },
+
+    ])
+    .then((answers) => {
+        console.log(answers)
+    });
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
